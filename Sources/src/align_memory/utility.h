98c6c7df52f485e038a4b4304dc6788727d67b18
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-12 15:05:43 +0530

    Create utility.h

---
+++ Sources/src/align_memory/utility.h
@@ -0,0 +1,8 @@
+#ifndef _KDI_ALIGN_MEMORY_UTILITY_H_
+#define _KDI_ALIGN_MEMORY_UTILITY_H_
+
+#include <stdint.h>
+
+#define ___KDI_ALIGN_MEMORY_OFFSET_SIZE sizeof(uint16_t)
+
+#endif  /* _KDI_ALIGN_MEMORY_H_ */
