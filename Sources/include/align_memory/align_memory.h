836e40e290dd4af75539e37fa2f0e0327cac52db
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-12 15:08:50 +0530

    Adding API function declarations
    Function declarations added:
    kdi_align_memory_get_allocation_size
    kdi_align_memory_get_aligned_address
    kdi_align_memory_get_actual_address

--- Sources/include/align_memory/align_memory.h
+++ Sources/include/align_memory/align_memory.h
@@ -3,6 +3,17 @@
 
 #include "align_memory_api.h"
 
-void KDI_ALIGN_MEMORY_API get_greeting(void);
+#include <stdint.h>
+
+uint32_t KDI_ALIGN_MEMORY_API
+    kdi_align_memory_get_allocation_size(uint32_t uiActual_size,
+                                         uint32_t uiAlignment_size);
+
+void KDI_ALIGN_MEMORY_API *
+    kdi_align_memory_get_aligned_address(void * pMemory,
+                                         uint32_t uiAlignment_size);
+
+void KDI_ALIGN_MEMORY_API *
+    kdi_align_memory_get_actual_address(void * pMemory);
 
 #endif  /* _KDI_ALIGN_MEMORY_H_ */
