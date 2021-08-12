83cfa738853feebffa1045a0d0a976722ba7a451
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-12 15:10:07 +0530

    Adding API function definitions
    Function definitions added:
    kdi_align_memory_get_allocation_size
    kdi_align_memory_get_aligned_address
    kdi_align_memory_get_actual_address

--- Sources/src/align_memory/align_memory.c
+++ Sources/src/align_memory/align_memory.c
@@ -1,8 +1,29 @@
 #include "align_memory/align_memory.h"
+#include "utility.h"
 
 #include <stdio.h>
 
-void get_greeting(void)
+uint32_t kdi_align_memory_get_allocation_size(uint32_t uiActual_size,
+                                              uint32_t uiAlignment_size)
 {
-    printf("Greetings!!! Kumarjit Das here.\n");
+    return --uiAlignment_size + uiActual_size +
+           ___KDI_ALIGN_MEMORY_OFFSET_SIZE;
+}
+
+void * kdi_align_memory_get_aligned_address(void *   pMemory,
+                                            uint32_t uiAlignment_size)
+{
+    --uiAlignment_size;
+    void * pTemporary_memory = pMemory;
+    pMemory = (void *) ((uintmax_t) ((uintmax_t) pMemory + uiAlignment_size +
+                         ___KDI_ALIGN_MEMORY_OFFSET_SIZE) &
+                        ~((uintmax_t) uiAlignment_size));
+    *((uint16_t *) pMemory - 1) = (uint16_t) ((uintmax_t) pMemory -
+                                              (uintmax_t) pTemporary_memory);
+    return pMemory;
+}
+
+void * kdi_align_memory_get_actual_address(void * pMemory)
+{
+    return (void *) ((uint8_t *) pMemory - *((uint16_t *) pMemory - 1));
 }
