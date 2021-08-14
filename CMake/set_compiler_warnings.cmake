7108b9a67881d998507e31604836b890bab78bce
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-14 23:39:30 +0530

    Adding compiler warnings for MSVC, Clang, and GCC

--- CMake/set_compiler_warnings.cmake
+++ CMake/set_compiler_warnings.cmake
@@ -1,12 +1,21 @@
 message(STATUS "${MY_STATUS_VARIABLE} Setting compiler warnings.")
 
-# Set the `MY_COMPILER_WARNING_OPTION` to all or to the maximum value for
-# different compilers
-string(CONCAT MY_COMPILER_WARNING_OPTION
-       "$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,"
-       "$<CXX_COMPILER_ID:GNU>>:-Wall -Wextra -pedantic -Werror>"
-       "$<$<CXX_COMPILER_ID:MSVC>:/W4 /WX>")
+string(CONCAT MSVC_WARNINGS "/WX /W4 /w14242 /w14254 /w14263 /w14265 /w14287 "
+                            "/we4289 /w14296 /w14311 /w14545 /w14546 /w14547 "
+                            "/w14549 /w14555 /w14619 /w14640 /w14826 /w14905 "
+                            "/w14906 /w14928 /permissive-")
+
+set(CLANG_WARNINGS "-Wall -Wextra -Wpedantic -Werror")
+
+set(GCC_WARNINGS "${CLANG_WARNINGS}")
 
 # Setting compiler warnings to the main target
-target_compile_options("${MY_LIBRARY_NAME}" PRIVATE
-                       "${MY_COMPILER_WARNING_OPTION}")
+if(MSVC)
+    target_compile_options("${MY_LIBRARY_NAME}" PRIVATE "${MSVC_WARNINGS}")
+elseif(CMAKE_C_COMPILER_ID MATCHES ".*Clang")
+    target_compile_options("${MY_LIBRARY_NAME}" PRIVATE "${CLANG_WARNINGS}")
+elseif(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    target_compile_options("${MY_LIBRARY_NAME}" PRIVATE "${GCC_WARNINGS}")
+else()
+    message(AUTHOR_WARNING "No compiler warnings set for ${MY_LIBRARY_NAME}")
+endif()
