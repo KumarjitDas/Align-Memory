ffbe611c29b8858bce55b07ee308e4f5f3bde190
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-23 11:04:43 +0530

    Removing quotes

--- CMake/set_include_directories.cmake
+++ CMake/set_include_directories.cmake
@@ -3,10 +3,10 @@ message(STATUS "${MY_STATUS_VARIABLE} Setting include directories.")
 target_include_directories(
     ${MY_LIBRARY_NAME} PUBLIC
     # Adding include directory for public inclusion
-    "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/Sources/include>"
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/Sources/include>
     # Adding the current binary directory to include path to include the
     # generated export header and configured version header
-    "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>"
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
     # Setting the install location of the above include files
-    "$<INSTALL_INTERFACE:include>"
+    $<INSTALL_INTERFACE:include>
 )
