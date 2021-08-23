5bf5a8033c54ac73231a82cae4317d0a6d3d94a4
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-08-23 11:03:31 +0530

    Adding more compiler options
    Adding compiler options for clang and gnu c compilers.

--- CMake/set_compiler_warnings.cmake
+++ CMake/set_compiler_warnings.cmake
@@ -1,13 +1,17 @@
 message(STATUS "${MY_STATUS_VARIABLE} Setting compiler warnings.")
 
-string(CONCAT MSVC_WARNINGS "/WX /W4 /w14242 /w14254 /w14263 /w14265 /w14287 "
-                            "/we4289 /w14296 /w14311 /w14545 /w14546 /w14547 "
-                            "/w14549 /w14555 /w14619 /w14640 /w14826 /w14905 "
-                            "/w14906 /w14928 /permissive-")
+string(CONCAT MSVC_WARNINGS /WX /W4 /w14242 /w14254 /w14263 /w14265 /w14287
+                            /we4289 /w14296 /w14311 /w14545 /w14546 /w14547
+                            /w14549 /w14555 /w14619 /w14640 /w14826 /w14905
+                            /w14906 /w14928 /permissive-)
 
-set(CLANG_WARNINGS "-Wall -Wextra -Wpedantic -Werror")
+set(CLANG_WARNINGS -Werror -Wall -Wextra -Wpedantic -Wshadow -Wcast-align
+                   -Wnon-virtual-dtor -Wold-style-cast -Woverloaded-virtual
+                   -Wconversion -Wsign-conversion -Wdouble-promotion -Wunused
+                   -Wnull-dereference)
 
-set(GCC_WARNINGS ${CLANG_WARNINGS})
+set(GCC_WARNINGS ${CLANG_WARNINGS} -Wmisleading-indentation -Wduplicated-cond
+                 -Wduplicated-branches -Wlogical-op -Wuseless-cast)
 
 # Setting compiler warnings to the main target
 if(MSVC)
