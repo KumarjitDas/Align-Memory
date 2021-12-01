5ea1a5e53073271ee4b6657b9acc921aa7b27755
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-12-01 10:23:11 +0530

    Added license and used `write_status`
    Adding a brief and license text at the top of the file as comments.
    Using `write_status` function instead of normal `message(STATUS...` function.
    Using KDI namespace variables instead of the double underscore ones.

--- CMake/add_sources_to_target.cmake
+++ CMake/add_sources_to_target.cmake
@@ -1,5 +1,34 @@
-message(STATUS
-        "${__STATUS_VARIABLE} Adding sources to the main library target.")
+##
+# file: add_sources_to_target.cmake
+# author: Kumarjit Das (kumarjitdas1999@gmail.com)
+# brief: Add the C source files to the project library.
+# version: 0.5.0
+# date: 2021-12-01
+#
+# License(MIT License):
+#
+# Copyright (c) 2021 Kumarjit Das | কুমারজিৎ দাস
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+write_status("Adding sources to the main library target.")
 
 # Setting the full path to Sources directory in SOURCES_DIRECTORY variable
 set(SOURCES_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/Sources")
@@ -8,12 +37,12 @@ set(SRC_DIRECTORY "${SOURCES_DIRECTORY}/src")
 
 # Adding the include file to the main target
 set(INCLUDE_FILE "${INCLUDE_DIRECTORY}/align_memory/align_memory.h")
-target_sources(${__LIBRARY_NAME} PRIVATE ${INCLUDE_FILE})
+target_sources(${KDI_LIBRARY_NAME} PRIVATE ${INCLUDE_FILE})
 
 # Adding the source files to the main target
 set(SRC_FILES "${SRC_DIRECTORY}/align_memory/align_memory.c"
               "${SRC_DIRECTORY}/align_memory/utility.h")
-target_sources(${__LIBRARY_NAME} PRIVATE ${SRC_FILES})
+target_sources(${KDI_LIBRARY_NAME} PRIVATE ${SRC_FILES})
 
 # Setting the install destination for include and source file
 install(FILES ${INCLUDE_FILE} DESTINATION "include/align_memory")
